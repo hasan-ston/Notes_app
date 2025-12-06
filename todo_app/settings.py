784998508c3f5a858b35f978beaba61df46de131
hasan-ston <mhd.hasan236@gmail.com>
2025-12-06 02:39:20 -0500

    Add 'Notes_app.onrender.com' to ALLOWED_HOSTS

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-qoc411r3)a6aurenhthfe2bn)^1k&448mu@whfmcky4uo+26pc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hasan221.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hasan221.pythonanywhere.com', '127.0.0.1', 'Notes_app.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
